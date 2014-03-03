1c2d9f40f86f90381a200b2695f2edd70c55c552
Nick Mitrou <nmitrou@sfu.ca>
2014-03-02 16:30:18 -0800

    Started run all script

---
+++ Matlab_codes/DMNO_run_all.m
@@ -0,0 +1,16 @@
+%% concatenate data
+    % BP
+    % RBF
+    % LS
+    % transfer functions
+    % LS transfer functions
+    % PC means
+    % PD means
+    % Cluster numbers
+%% load concatenated data
+%% generate plots
+    % BP, RBF, LS
+    % transfer functions
+    % Cluster imagte
+        % Cluster map, PC map, PD map
+%% export plots
